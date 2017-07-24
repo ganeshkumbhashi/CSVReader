76eff315568e265aa75d5090aa7b5ad830a8dba9
geekkdev <ganeshkr2005@gmail.com>
2017-07-24 17:52:15 +0530

    changed csv file path

--- src/com/geeksdev/csv/CSVReader.java
+++ src/com/geeksdev/csv/CSVReader.java
@@ -8,7 +8,7 @@ public class CSVReader
 {
 	public static void main(String[] args)
 	{
-		String csvFile = "C:\\Users\\Admin\\Desktop\\dsconfiguration.csv";
+		String csvFile = "d:\\test.csv";
 		String line = "";
 		String cvsSplitBy = ",";
 
